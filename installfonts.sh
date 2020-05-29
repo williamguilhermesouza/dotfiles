d32c58ed03e8e845e73851dd1aa900730638497b
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-05-29 17:08:15 -0300

    created scripts for symlinking dotfiles

---
+++ installfonts.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# cp fonts firacode folder to home/.local/share, if fonts already exist create fonts inside it
+cp -r ./fonts ~/.local/share/fonts
---
+++ symlinkcreation.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+## VIM CONFIGURATION ##
+# create init.vim symlink
+ln -sfi $PWD/init.vim ~/.config/nvim/init.vim
+# create coc.vim symlink
+ln -sfi $PWD/coc.vim ~/.config/nvim/coc.vim
+# create plugins.vim symlink
+ln -sfi $PWD/plugins.vim ~/.config/nvim/plugins.vim
