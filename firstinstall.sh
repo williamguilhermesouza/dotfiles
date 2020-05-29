a668f0acb58134af35254ae355aeb217404f0903
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-05-29 20:05:06 -0300

    instalation of nvim

--- firstinstall.sh
+++ firstinstall.sh
@@ -17,6 +17,9 @@ cd ..
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
 
 git clone "https://github.com/williamguilhermesouza/dotfiles.git"
 cd dotfiles
