664358b32d5d1fe1dd057f6dff718a802f43b09d
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-05-29 19:16:54 -0300

    created script for zsh and other confs

--- firstinstall.sh
+++ firstinstall.sh
@@ -8,4 +8,18 @@
 # As an alternative, you can first download the install script and run it afterwards:
 #   wget  https://raw.githubusercontent.com/williamguilhermesouza/dotfiles/master/firstinstall.sh)
 # install.sh
-ls
+sudo apt install fonts-firacode zsh dconf-cli silversearcher-ag -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/dracula/gnome-terminal
+cd gnome-terminal
+./install.sh
+cd ..
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+
+git clone "https://github.com/williamguilhermesouza/dotfiles.git"
+cd dotfiles
+./installfonts.sh
+./symlinkcreation.sh
+
--- symlinkcreation.sh
+++ symlinkcreation.sh
@@ -7,3 +7,6 @@ ln -sfi $PWD/init.vim ~/.config/nvim/init.vim
 ln -sfi $PWD/coc.vim ~/.config/nvim/coc.vim
 # create plugins.vim symlink
 ln -sfi $PWD/plugins.vim ~/.config/nvim/plugins.vim
+## TERMINAL CONFIGURATION ## 
+# create .zshrc symlink
+ln -sfi $PWD/.zshrc ~/.zshrc
