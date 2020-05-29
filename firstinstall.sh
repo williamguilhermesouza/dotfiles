c124e926334484c10486fa5fdaced9b17ef1482b
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-05-29 20:38:49 -0300

    vimplug install

--- firstinstall.sh
+++ firstinstall.sh
@@ -18,6 +18,7 @@ git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/theme
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 [ ! -d "~/.config/nvim" ] && mkdir -p ~/.config/nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 git clone "https://github.com/williamguilhermesouza/dotfiles.git"
 cd dotfiles
 ./installfonts.sh
