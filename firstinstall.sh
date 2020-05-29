e98f77ae44e5e4276c6462b6f23760427eb56bcd
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-05-29 20:34:18 -0300

    neovim install

--- firstinstall.sh
+++ firstinstall.sh
@@ -8,7 +8,7 @@
 # As an alternative, you can first download the install script and run it afterwards:
 #   wget  https://raw.githubusercontent.com/williamguilhermesouza/dotfiles/master/firstinstall.sh)
 # install.sh
-sudo apt install fonts-firacode zsh dconf-cli silversearcher-ag -y
+sudo apt install fonts-firacode zsh dconf-cli silversearcher-ag neovim -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/dracula/gnome-terminal
 cd gnome-terminal
@@ -17,9 +17,6 @@ cd ..
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage
 [ ! -d "~/.config/nvim" ] && mkdir -p ~/.config/nvim
 git clone "https://github.com/williamguilhermesouza/dotfiles.git"
 cd dotfiles
