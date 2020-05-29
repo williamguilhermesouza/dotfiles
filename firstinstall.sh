c6335e54251ac12954a0e34f6dbe3c050e8dbfcf
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-05-29 18:09:35 -0300

    testing script for direct github install

---
+++ firstinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# This script should be run via curl:
+#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/williamguilhermesouza/dotfiles/master/firstinstall.sh)"
+# or wget:
+#   sh -c "$(wget -qO- https://raw.githubusercontent.com/williamguilhermesouza/dotfiles/master/firstinstall.sh)"
+#
+# As an alternative, you can first download the install script and run it afterwards:
+#   wget  https://raw.githubusercontent.com/williamguilhermesouza/dotfiles/master/firstinstall.sh)
+# install.sh
+echo "teste"
