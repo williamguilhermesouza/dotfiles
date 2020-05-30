e061100a5c144a146b7dd57d4cff0b2fdaeb8ff9
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-05-29 23:14:42 -0300

    dracula alts

--- firstinstall.sh
+++ firstinstall.sh
@@ -13,6 +13,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt install gconf-service fonts-firacode zsh dconf-cli silversearcher-ag nodejs neovim -y
 sudo apt update && sudo apt install --no-install-recommends yarn
+sudo npm install npm@latest -g
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo git clone https://github.com/dracula/zsh.git
 sudo ln -s $PWD/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
