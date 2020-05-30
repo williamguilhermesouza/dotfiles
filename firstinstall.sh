6e051356ed6aa6ad9eab4d9fbebd9325455de70b
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-05-29 22:27:02 -0300

    removing oh-my-zsh

--- firstinstall.sh
+++ firstinstall.sh
@@ -20,13 +20,12 @@ cd gnome-terminal
 ./install.sh
 cd ..
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "~/.oh-my-zsh/themes/spaceship.zsh-theme"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 [ ! -d "~/.config/nvim" ] && mkdir -p ~/.config/nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 git clone "https://github.com/williamguilhermesouza/dotfiles.git"
 cd dotfiles
-cp -r $PWD/.oh-my-zsh ~/.oh-my-zsh
 ./installfonts.sh
 ./symlinkcreation.sh
 
