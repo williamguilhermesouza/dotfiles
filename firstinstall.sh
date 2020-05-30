fd4bcb4544444a03900e835033db1553d266965b
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-05-29 22:29:16 -0300

    removing oh-my-zsh

--- firstinstall.sh
+++ firstinstall.sh
@@ -12,7 +12,7 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install gconftool-2 fonts-firacode zsh dconf-cli silversearcher-ag nodejs neovim -y
+sudo apt install gconf fonts-firacode zsh dconf-cli silversearcher-ag nodejs neovim -y
 sudo apt update && sudo apt install --no-install-recommends yarn
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/dracula/gnome-terminal
