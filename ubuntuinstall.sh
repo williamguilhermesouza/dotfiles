92f81b9c0f59a273cbb8721d07d710527f953be1
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-10-27 20:55:37 -0300

    changed firstinstall to ubuntuinstall

--- ubuntuinstall.sh
+++ ubuntuinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 # This script should be run via curl:
-#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/williamguilhermesouza/dotfiles/master/firstinstall.sh)"
+#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/williamguilhermesouza/dotfiles/master/ubuntuinstall.sh)"
 # or wget:
-#   sh -c "$(wget -qO- https://raw.githubusercontent.com/williamguilhermesouza/dotfiles/master/firstinstall.sh)"
+#   sh -c "$(wget -qO- https://raw.githubusercontent.com/williamguilhermesouza/dotfiles/master/ubuntuinstall.sh)"
 #
 # As an alternative, you can first download the install script and run it afterwards:
 #   wget  https://raw.githubusercontent.com/williamguilhermesouza/dotfiles/master/firstinstall.sh)
