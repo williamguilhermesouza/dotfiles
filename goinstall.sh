688250844f480fafca7ee614fa0173fd6409ca7a
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-12-12 15:53:03 -0300

    added fix to goinstall

--- goinstall.sh
+++ goinstall.sh
@@ -18,7 +18,7 @@ sudo tar -C /usr/local -xzf go1.15.3.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 # add export to bashrc
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
 
 # check version 
 go version
