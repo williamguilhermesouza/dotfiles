d95048b7f9cacc0bf4e5b1dec5c8f4e492da4b26
William Souza <williamguilhermesouza@gmail.com>
2026-03-29 15:23:09 -0300

    adding gi shortcut in nvim

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,181 +1,182 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {},
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {},
+        },
     },
-  },
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    opts = {},
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "williamboman/mason.nvim" },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/nvim-cmp",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "j-hui/fidget.nvim",
+    {
+        "williamboman/mason.nvim",
+        cmd = "Mason",
+        opts = {},
     },
-    config = function()
-      require("conform").setup({
-        formatters_by_ft = {},
-      })
+    {
+        "williamboman/mason-lspconfig.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = { "williamboman/mason.nvim" },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/nvim-cmp",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "j-hui/fidget.nvim",
+        },
+        config = function()
+            require("conform").setup({
+                formatters_by_ft = {},
+            })
 
-      local cmp = require("cmp")
-      local cmp_lsp = require("cmp_nvim_lsp")
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend("force", capabilities, cmp_lsp.default_capabilities())
-      vim.lsp.config("*", {
-        capabilities = capabilities,
-      })
+            local cmp = require("cmp")
+            local cmp_lsp = require("cmp_nvim_lsp")
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend("force", capabilities, cmp_lsp.default_capabilities())
+            vim.lsp.config("*", {
+                capabilities = capabilities,
+            })
 
-      require("fidget").setup({})
-      require("mason").setup()
+            require("fidget").setup({})
+            require("mason").setup()
 
-      local ok_mason_lsp, mason_lspconfig = pcall(require, "mason-lspconfig")
-      local servers = {
-        "lua_ls",
-        "vtsls",
-        "tailwindcss",
-        "pyright",
-        "jsonls",
-        "omnisharp",
-      }
-      if ok_mason_lsp then
-        mason_lspconfig.setup({
-          ensure_installed = servers,
-          automatic_enable = false,
-        })
-      end
+            local ok_mason_lsp, mason_lspconfig = pcall(require, "mason-lspconfig")
+            local servers = {
+                "lua_ls",
+                "vtsls",
+                "tailwindcss",
+                "pyright",
+                "jsonls",
+                "omnisharp",
+            }
+            if ok_mason_lsp then
+                mason_lspconfig.setup({
+                    ensure_installed = servers,
+                    automatic_enable = false,
+                })
+            end
 
-      local function setup_server(server, opts)
-        opts = opts or {}
-        opts.capabilities = vim.tbl_deep_extend("force", capabilities, opts.capabilities or {})
+            local function setup_server(server, opts)
+                opts = opts or {}
+                opts.capabilities = vim.tbl_deep_extend("force", capabilities, opts.capabilities or {})
 
-        local ok_config = pcall(vim.lsp.config, server, opts)
-        if not ok_config then
-          vim.notify("LSP config not found for: " .. server, vim.log.levels.WARN)
-          return
-        end
+                local ok_config = pcall(vim.lsp.config, server, opts)
+                if not ok_config then
+                    vim.notify("LSP config not found for: " .. server, vim.log.levels.WARN)
+                    return
+                end
 
-        local ok_enable = pcall(vim.lsp.enable, server)
-        if not ok_enable then
-          vim.notify("Failed to enable LSP server: " .. server, vim.log.levels.WARN)
-        end
-      end
+                local ok_enable = pcall(vim.lsp.enable, server)
+                if not ok_enable then
+                    vim.notify("Failed to enable LSP server: " .. server, vim.log.levels.WARN)
+                end
+            end
 
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local opts = { buffer = args.buf, remap = false }
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "<leader>vws", function()
-            vim.ui.input({ prompt = "Workspace Symbol > " }, function(query)
-              if query and query ~= "" then
-                vim.lsp.buf.workspace_symbol(query)
-              end
-            end)
-          end, opts)
-          vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-          vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-          vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-          vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-          vim.keymap.set("n", "<leader>f", function()
-            require("conform").format({ async = true, lsp_fallback = true })
-          end, opts)
-          vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    local opts = { buffer = args.buf, remap = false }
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "<leader>vws", function()
+                        vim.ui.input({ prompt = "Workspace Symbol > " }, function(query)
+                            if query and query ~= "" then
+                                vim.lsp.buf.workspace_symbol(query)
+                            end
+                        end)
+                    end, opts)
+                    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+                    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+                    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+                    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("n", "<leader>f", function()
+                        require("conform").format({ async = true, lsp_fallback = true })
+                    end, opts)
+                    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+                end,
+            })
+
+            setup_server("lua_ls", {
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        runtime = { version = "LuaJIT" },
+                        diagnostics = { globals = { "vim" } },
+                        workspace = {
+                            library = vim.api.nvim_get_runtime_file("", true),
+                            checkThirdParty = false,
+                        },
+                        format = {
+                            enable = true,
+                            defaultConfig = {
+                                indent_style = "space",
+                                indent_size = "2",
+                            },
+                        },
+                    },
+                },
+            })
+
+            setup_server("tailwindcss", {
+                capabilities = capabilities,
+                filetypes = {
+                    "html",
+                    "css",
+                    "scss",
+                    "javascript",
+                    "javascriptreact",
+                    "typescript",
+                    "typescriptreact",
+                    "vue",
+                    "svelte",
+                    "heex",
+                },
+            })
+
+            setup_server("vtsls", { capabilities = capabilities })
+            setup_server("pyright", { capabilities = capabilities })
+            setup_server("jsonls", { capabilities = capabilities })
+            setup_server("omnisharp", { capabilities = capabilities })
+
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+                    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                }, {
+                    { name = "buffer" },
+                }),
+            })
+
+            vim.diagnostic.config({
+                float = {
+                    focusable = false,
+                    style = "minimal",
+                    border = "rounded",
+                    source = "always",
+                    header = "",
+                    prefix = "",
+                },
+            })
         end,
-      })
-
-      setup_server("lua_ls", {
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            runtime = { version = "LuaJIT" },
-            diagnostics = { globals = { "vim" } },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false,
-            },
-            format = {
-              enable = true,
-              defaultConfig = {
-                indent_style = "space",
-                indent_size = "2",
-              },
-            },
-          },
-        },
-      })
-
-      setup_server("tailwindcss", {
-        capabilities = capabilities,
-        filetypes = {
-          "html",
-          "css",
-          "scss",
-          "javascript",
-          "javascriptreact",
-          "typescript",
-          "typescriptreact",
-          "vue",
-          "svelte",
-          "heex",
-        },
-      })
-
-      setup_server("vtsls", { capabilities = capabilities })
-      setup_server("pyright", { capabilities = capabilities })
-      setup_server("jsonls", { capabilities = capabilities })
-      setup_server("omnisharp", { capabilities = capabilities })
-
-      local cmp_select = { behavior = cmp.SelectBehavior.Select }
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-          ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-Space>"] = cmp.mapping.complete(),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-        }),
-      })
-
-      vim.diagnostic.config({
-        float = {
-          focusable = false,
-          style = "minimal",
-          border = "rounded",
-          source = "always",
-          header = "",
-          prefix = "",
-        },
-      })
-    end,
-  },
+    },
 }
