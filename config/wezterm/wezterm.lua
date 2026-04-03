b89eeebd1af271bcc2a157d21309fa597ef2a3dc
William Souza <williamguilhermesouza@gmail.com>
2026-04-03 15:18:51 -0300

    working on wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,16 +1,20 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 local config = wezterm.config_builder()
+local startup_dir = os.getenv("DEV_ENV") or os.getenv("HOME") or wezterm.home_dir
 
 -- Start with PowerShell Core
 config.default_prog = { "pwsh.exe" }
+config.default_cwd = startup_dir
 
 -- No borders, no tab bar
 config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
 config.enable_tab_bar = false
 
 config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
-config.font_size = 10
+config.font_size = 12
+
 
 -- Color scheme — high contrast, easy on projectors
 config.color_scheme = "Tokyo Night Storm"
@@ -26,5 +30,17 @@ config.window_padding = {
 -- Hide scrollbar, steady block cursor
 config.enable_scroll_bar = false
 config.default_cursor_style = "SteadyBlock"
+config.keys = {
+  { key = "1", mods = "CTRL", action = wezterm.action.ActivateTab(0) },
+  { key = "2", mods = "CTRL", action = wezterm.action.ActivateTab(1) },
+  { key = "3", mods = "CTRL", action = wezterm.action.ActivateTab(2) },
+  { key = "4", mods = "CTRL", action = wezterm.action.ActivateTab(3) },
+  { key = "5", mods = "CTRL", action = wezterm.action.ActivateTab(4) },
+  { key = "6", mods = "CTRL", action = wezterm.action.ActivateTab(5) },
+  { key = "7", mods = "CTRL", action = wezterm.action.ActivateTab(6) },
+  { key = "8", mods = "CTRL", action = wezterm.action.ActivateTab(7) },
+  { key = "9", mods = "CTRL", action = wezterm.action.ActivateTab(8) },
+  { key = "0", mods = "CTRL", action = wezterm.action.ActivateTab(-1) },
+}
 
 return config
