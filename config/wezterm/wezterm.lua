f0dfa23bb43152863de958e66720d67fad25bd90
williamguilhermesouza <williamguilhermesouza@gmail.com>
2026-06-30 20:17:08 -0300

    automated dev commit

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -26,8 +26,8 @@ config.unix_domains = {
 -- No borders, no tab bar
 config.window_decorations = "NONE"
 config.hide_tab_bar_if_only_one_tab = true
-config.tab_bar_at_bottom = true
-config.use_fancy_tab_bar = true
+-- config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
 
 config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
 config.font_size = 12
