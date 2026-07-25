df3c5bc3422e54c9d17ec059314dd99f37c71bce
williamguilhermesouza <williamguilhermesouza@gmail.com>
2026-07-25 12:54:15 -0300

    toggleterm only on normal mode

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -4,7 +4,7 @@ return {
 	-- cmd = "ToggleTerm",
 	keys = {
 
-		{ '<leader>t',     '<cmd>:1ToggleTerm direction=float<CR>',              mode = { 'n', 'i', 't' } },
+		{ '<leader>t',     '<cmd>:1ToggleTerm direction=float<CR>',              mode = { 'n', 't' } },
 		{ 'gt',     '<cmd>:2ToggleTerm direction=horizontal size=20<CR>', mode = { 'n', 't' } },
 		{ 'gT',     '<cmd>:3ToggleTerm direction=vertical size=100<CR>',  mode = { 'n', 't' } },
 		{ '<leader>T',     '<cmd>:4ToggleTerm direction=float<CR>',              mode = { 'n', 't' } },
