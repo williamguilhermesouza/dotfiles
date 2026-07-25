d9c45bfc1d69685d110c4803cc89b73844d0c5c3
williamguilhermesouza <williamguilhermesouza@gmail.com>
2026-07-25 13:55:48 -0300

    toggleterm config

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -4,10 +4,12 @@ return {
 	-- cmd = "ToggleTerm",
 	keys = {
 
-		{ '<leader>t',     '<cmd>:1ToggleTerm direction=float<CR>',              mode = { 'n', 't' } },
+		{ '<leader>t',     '<cmd>:1ToggleTerm direction=float<CR>',              mode = { 'n' } },
+		{ '<C-c>',     '<cmd>:1ToggleTerm direction=float<CR>',              mode = { 't' } },
 		{ 'gt',     '<cmd>:2ToggleTerm direction=horizontal size=20<CR>', mode = { 'n', 't' } },
 		{ 'gT',     '<cmd>:3ToggleTerm direction=vertical size=100<CR>',  mode = { 'n', 't' } },
-		{ '<leader>T',     '<cmd>:4ToggleTerm direction=float<CR>',              mode = { 'n', 't' } },
+		{ '<leader>T',     '<cmd>:4ToggleTerm direction=float<CR>',              mode = { 'n' } },
+		{ '<C-c>',     '<cmd>:4ToggleTerm direction=float<CR>',              mode = { 't' } },
 		{ '<leader>gl', function() end,                                       mode = { 'n', } },
 		{ '<leader>fr', function() end,                                       mode = { 'n', } },
 	},
